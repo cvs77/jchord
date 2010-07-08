26a5e2c1c1533c8fbc7d6df7c0a9d1d67442b279
omertripp <omertripp@fd19b882-93bd-11dd-bc7b-59c4a08ba726>
2010-07-08 10:58:52 +0000

    FEAT: - test case to verify that the refinement alg. is doing the right thing.

--- branches/refine/examples/datarace_test/src/test/T.java
+++ branches/refine/examples/datarace_test/src/test/T.java
@@ -6,39 +6,50 @@
 package test;
 
 /**
- * @author Mayur Naik (mhn@cs.stanford.edu)
+ * @author Omer Tripp (omertrip@post.tau.ac.il)
  */
 public class T extends java.lang.Thread {
-	static Object p;
-	static {
-		p = new Object();
+	
+	private B b1, b2;
+	
+	static class B {
+		int f;
+		
+		public static B getB1() {
+//			return new B();
+			return getB2();
+		}
+		
+		private static B getB2() {
+			return new B();
+		}
 	}
+	
+	public T(B b1, B b2) {
+		this.b1 = b1;
+		this.b2 = b2;
+	}
+	
     public static void main(String[] a) {
-        B b1 = new B();
-        B b2 = new B();
-        for (int i = 0; i < 10; i++) {
-            T t = new T(b1, b2);
-            t.start();
-        }
-        for (int i = 0; i < 10; i++) {
-            b1.get();
-            synchronized (b2) {
-                b2.set(i);
-            }
-        }
+        B b1 = B.getB1();
+        B b2 = B.getB1();
+        T t1 = new T(b1, b2);
+//        T t2 = new T(b1, b2);
+        t1.start();
+//        t2.start();
     }
-    private B f1, f2;
-    public T(B b1, B b2) {
-        this.f1 = b1;
-        this.f2 = b2;
-    }
+    
+    @Override
     public void run() {
-        B b1 = this.f1;
-        B b2 = this.f2;
-        int i;
-        synchronized (b2) {
-            i = b2.get();
-        }
-        b1.set(i);
+    	foo();
+    	bar();
+    }
+    
+    private void foo() {
+    	b1.f = 3;
+    }
+    
+    private void bar() {
+    	b2.f = 3;
     }
 }
